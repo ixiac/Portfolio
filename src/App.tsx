35a364b3f90bba062e7dd14af33ec90cadccd0e2
bieners <bienmarlon.qm@gmail.com>
2026-01-31 02:20:07 +0800

    Page section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+import { Hero } from "@/components/Hero";
+import { About } from "@/components/About.tsx";
+import { Projects } from "@/components/Projects.tsx";
+import { Resume } from "@/components/Resume.tsx";
+
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  )
+    <main className="flex flex-col container mx-auto p-10 max-w-4xl lg:pr-0 lg:pl-0 lg:max-w-6xl">
+      <Hero />
+      <About />
+      <Projects />
+      <Resume />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import App from "@/App.tsx";
 import { Sidebar } from "@/components/Sidebar.tsx";
+import { Profile } from "@/components/Profile.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10">
+    <div className="min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10 bg-neutral-900">
       <Sidebar />
       <Profile />
       <App />
