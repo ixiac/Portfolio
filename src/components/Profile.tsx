0358a7d391789eaa3d795a1f9a56fdfb2758b4fb
bieners <bienmarlon.qm@gmail.com>
2026-01-30 18:21:24 +0800

    Profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./ui/button";
+import { socialLinks } from "@/constants";
+
+export const Profile = () => {
+  return (
+    <aside className="">
+      <div className="">
+        <div className="">
+          <h1 className="">Bien</h1>
+            <p className="">Developer & Analyst</p>
+        </div>
+      </div>
+    </aside>
+  );
+};
