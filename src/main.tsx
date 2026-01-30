1aa54f6bec99578f0b6291c38b4e82018bb9d68c
bieners <bienmarlon.qm@gmail.com>
2026-01-30 18:19:10 +0800

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@/index.css";
+import App from "@/App.tsx";
+import { Sidebar } from "@/components/Sidebar.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <div className="min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10">
+      <Sidebar />
+      <Profile />
+      <App />
+    </div>
+  </StrictMode>,
+);
