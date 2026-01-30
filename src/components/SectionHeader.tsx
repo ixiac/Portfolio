255c7cd3c7a180abd202410f8dd73fbb706cb046
bieners <bienmarlon.qm@gmail.com>
2026-01-31 02:21:26 +0800

    Header for each section

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,29 @@
+import { motion } from "motion/react";
+import { fadeUp } from "@/constants/animations";
+import { SparkleIcon } from "lucide-react";
+
+export const SectionHeader = ({
+  title,
+  subtitle,
+}: {
+  title: string;
+  subtitle: string;
+}) => {
+  return (
+    <>
+      <motion.p
+        variants={fadeUp}
+        className="flex items-center justify-center py-1 gap-2 w-42 "
+      >
+        <SparkleIcon size={15} className="font-medium text-white mt-4" />{" "}
+        <span className="font-semibold text-white mt-4">{title}</span>
+      </motion.p>
+
+      <motion.h2 variants={fadeUp}>
+        <span className="text-4xl md:text-4xl lg:text-5xl font-medium capitalize mt-4 text-neutral-200">
+          {subtitle}
+        </span>
+      </motion.h2>
+    </>
+  );
+};
