725b98846bb8e170c13ec245833fb53e6b022a76
bieners <bienmarlon.qm@gmail.com>
2026-01-31 15:10:18 +0800

    Adjusted content

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
-import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
 import { Hero } from "@/components/Hero";
 import { About } from "@/components/About.tsx";
 import { Projects } from "@/components/Projects.tsx";
 import { Resume } from "@/components/Resume.tsx";
-
+import { Contact } from "@/components/Contact.tsx";
+import Lenis  from "lenis";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+    (window as any).lenis = lenis;
+    
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+    
+    return () => {
+      lenis.destroy();
+      delete (window as any).lenis;
+    };
+  }, []);
+  
   return (
     <main className="flex flex-col container mx-auto p-10 max-w-4xl lg:pr-0 lg:pl-0 lg:max-w-6xl">
       <Hero />
       <About />
       <Projects />
       <Resume />
+      <Contact />
     </main>
   );
 }
-
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,21 @@ import "@/index.css";
 import App from "@/App.tsx";
 import { Sidebar } from "@/components/Sidebar.tsx";
 import { Profile } from "@/components/Profile.tsx";
+import { Footer } from "@/components/Footer";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10 bg-neutral-900">
-      <Sidebar />
-      <Profile />
-      <App />
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="min-h-screen flex flex-col dark:bg-neutral-900 bg-neutral-200">
+        <div className="flex-1 lg:flex lg:justify-center lg:items-start lg:gap-10">
+          <Sidebar />
+          <Profile />
+          <App />
+        </div>
+        <Footer />
+      </div>
+    </ThemeProvider>
   </StrictMode>,
 );
